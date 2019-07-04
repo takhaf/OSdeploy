5dcbc8f8d38252ff0cb06cd8855c5091887a0d93
takhaf <takhaf@gmail.com>
2019-07-04 11:39:27 +0200

    2nd commit

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 pass=$1
 IP=$2
+type=$3
+if ["type" -eq "controller" ]
+	then source controller_actions.sh
+elif ["type" -eq "compute" ]
+	then source compute_actions.sh
+
+else echo "Unknown node type"
+fi
 
